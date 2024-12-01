1ebb58ea41b2df7eb6af0d40f84a9bd54d4240be
UASTRONGMAN <shkolniy.maks@ukr.net>
2024-12-02 00:01:55 +0200

    123

--- frontend/src/services/carService.js
+++ frontend/src/services/carService.js
@@ -1,11 +1,12 @@
 import {apiService} from "./apiService";
 import * as url from "node:url";
+import {urls} from "../constants/urls";
 
 const carService = {
     get_all_cars () {
-        return apiService.get(urls.cars.all_cars)
+        return apiService.get(urls.cars.cars)
     },
     create_car_ad(car) {
-        return apiService.post(urls.cars)
+        return apiService.post(urls.cars.create_ad)
     }
 }
